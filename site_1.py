acbd442f7562002160454122beca7773fca0b040
Jean Calão <127217371+jeancalao@users.noreply.github.com>
2024-06-28 07:28:08 -0300

    Add files via upload

--- site_1.py
+++ site_1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from time import sleep
 
-st.set_page_config(page_title='Minha Pagina',
+st.set_page_config(page_title='Página de Teste',
                 page_icon="🧊",
                 layout="centered",
                 initial_sidebar_state="expanded",
@@ -12,7 +12,7 @@ st.set_page_config(page_title='Minha Pagina',
                     'About': "# This is a header. This is an *extremely* cool app!"})
     
 with st.container():
-    st.subheader("Meu primeiro site com streamlit")
+    st.subheader("Site em Python utilizando Streamlite")
     st.title("Dashboard de Contratos")
     st.write("informações sobre os contratos")
     st.write("Quer aprender Python? [Clique Aqui](http://python.org)")
@@ -26,6 +26,7 @@ with st.container(border=True):
     st.write("---")
     #busca referencia na streamlit docs
     #grafico de área
+    st.caption(":blue[**Tabela de Contratos**] :dollar: utilizando arquivo .CSV")
     dados = carregar_dados()
     qtde_dias = st.selectbox("selecione o periodo",['7d','15d','21d'])
     qtde_dias = int(qtde_dias.replace('d',''))
@@ -35,13 +36,14 @@ with st.container(border=True):
 with st.container(border=True):
     w_base = pd.read_sql_table('Cotacoes',"sqlite:///projeto2.db")
     st.write("---")
-    st.caption(":red[**Tabela de Cotações**] :dollar:")
+    st.caption(":red[**Tabela de Cotações**] :dollar: utilizando BD SQLLite")
+    st.bar_chart(w_base, x="acao", y="cotacao")
     st.table(w_base)
     #st.error('This is an error', icon="🚨")
-    st.success('This is a success message!', icon="✅")
-    with st.spinner('Aguarde ...'):
-        sleep(5)
-    st.success('Pronto!', icon="✅")
+    #st.success('This is a success message!', icon="✅")
+    # with st.spinner('Aguarde ...'):
+    #     sleep(5)
+    
 
 with st.container():
     def cook_breakfast():
@@ -56,5 +58,22 @@ with st.container():
         st.balloons()
         st.snow()
     
-    st.help(st.area_chart)
-    st.help(pd.array)
+    #st.help(st.area_chart)
+    #st.help(pd.array)
+
+with st.container(border=True):
+    dados = pd.read_excel('clientes.xlsx')
+    st.write('---')
+    st.caption(":red[**Tabela de Clientes**] :dollar: utilizando Excel")
+    st.area_chart(dados, x="Cliente", y="Valor")
+    st.table(dados)
+
+st.success('Pronto!', icon="✅")
+on = st.toggle("Demonstrar dependências")
+
+if on:
+    st.write("**Necessário efetuar Deploy das seguintes bibliotecas:**")
+    st.write("SQLSchema - para efetuar a leitura da base de dados SQLLite")
+    st.write("openpyxl - para abrir o arquivo Excel")
+    st.write("Efetuar deploy também dos arquivos Excel, do banco SQLLite e do arquivo .CSV")
+    st.write("devem estar definidas no arquivo requirements.txt do deploy github")
